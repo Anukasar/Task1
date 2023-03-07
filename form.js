c4860d097b04561923d062ead22d38c761d53633
Anukasar <kasaranuja243@gmail.com>
2023-03-07 19:40:43 +0530

    First Commit

---
+++ form.js
@@ -0,0 +1,15 @@
+
+      // Form validation code will come here.
+      function validate() {
+      
+         if( document.myForm.Name.value == "" ) {
+            alert( "Please provide your name!" );
+            document.myForm.Name.focus() ;
+            return false;
+         }
+         if( document.myForm.EMail.value == "" ) {
+            alert( "Please provide your Email!" );
+            document.myForm.EMail.focus() ;
+            return false;
+         }
+    }
